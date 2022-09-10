211dc96e3cc73c9fd071376ac9d9d19f8684f4d0
Florent Chretien <flopubs@gmail.com>
2022-09-10 10:50:50 +0000

    first communicating lib

---
+++ include/serial_megapi.h
@@ -0,0 +1,59 @@
+#ifndef SERIAL_MEGAPI_H_
+#define SERIAL_MEGAPI_H_
+
+#define MAKEBLOCK_MSG_SIZE  0xa
+#define MKBLK_MAX_MSG_SIZE  0x14
+
+#define HEADER_MSG_SIZE     0x3
+
+#define GYRO_ALL_AXES       0x0
+#define GYRO_AXE_X          0x2
+#define GYRO_AXE_Y          0x1
+#define GYRO_AXE_Z          0x3
+#define GYRO_DEV_ID         0x6
+#define GYRO_MSG_SIZE       0x5
+#define GYRO_PORT           0x0
+
+#define USS_DEV_ID          0x1
+#define USS_MSG_SIZE        0x4
+#define USS_MAX_NB          0x4
+#define USS_FIRST_SLOT      0x5
+#define USS_LAST_SLOT       USS_FIRST_SLOT + USS_MAX_NB - 1
+
+#define DATA_TYPE_FLOAT     0x2
+
+#define ACTION_GET          0x1
+#define ACTION_RUN          0x2
+#define ACTION_RESET        0x4
+#define ACTION_START        0x5
+
+char makeblock_response_msg[MKBLK_MAX_MSG_SIZE];
+
+typedef struct SerialGyro
+{
+    float x_;
+    float y_;
+    float z_;
+} SerialGyro;
+
+typedef struct SerialUss
+{
+    char port;
+    float distance_cm;
+} SerialUss;
+
+SerialGyro data_gyro;
+SerialUss data_uss[8];
+
+float get_gyro_x();
+float get_gyro_y();
+float get_gyro_z();
+float get_uss(int port);
+
+int request_gyro_all_axes(const int fd);
+int request_gyro(const int fd, char axis);
+int request_uss(const int fd, char port);
+
+void receive_msg(const int fd);
+
+#endif // SERIAL_MEGAPI_H_
---
+++ src/serial_megapi.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#include "serial_megapi.h"
+
+int decode_data(){
+    // char ret = {0x0, 0x0, 0x0, 0x0}; 
+    int ret = -1;
+    if(0xff == makeblock_response_msg[0] && 0x55 == makeblock_response_msg[1] 
+        && 0xd == makeblock_response_msg[8] && 0xa == makeblock_response_msg[9])
+    {
+        char ext_id = makeblock_response_msg[2];
+        char data_type = makeblock_response_msg[3];
+        char dev_id = ext_id & 0xf;
+
+        // printf("dev_id : %d / data_type : %d", dev_id, data_type);
+
+        switch (dev_id) {
+            case USS_DEV_ID:
+                int port = ((ext_id & 0xf0) >> 4);
+                if(DATA_TYPE_FLOAT == data_type){
+                    data_uss[port-1].distance_cm = *(float*)(makeblock_response_msg+4);
+                    ret = 0;
+                }
+                break;
+            case GYRO_DEV_ID:
+                char axis = ((ext_id & 0xf0) >> 4) - GYRO_PORT;
+                if(DATA_TYPE_FLOAT == data_type){
+                    if(GYRO_AXE_X == axis){
+                        data_gyro.x_ = *(float*)(makeblock_response_msg+4);
+                        ret = 0;
+                    } else if(GYRO_AXE_Y == axis){
+                        data_gyro.y_ = *(float*)(makeblock_response_msg+4);
+                        ret = 0;
+                    } else if(GYRO_AXE_Z == axis){
+                        data_gyro.z_ = *(float*)(makeblock_response_msg+4);
+                        ret = 0;
+                    }
+                    
+                }
+
+            default:
+                break;
+        }
+    } else if (
+        0xff == makeblock_response_msg[0] && 0x55 == makeblock_response_msg[1] 
+        && 0xd == makeblock_response_msg[18] && 0xa == makeblock_response_msg[19])
+    {
+
+        char ext_id = makeblock_response_msg[2];
+        char data_type[3] = {makeblock_response_msg[3], makeblock_response_msg[8], makeblock_response_msg[13]};
+        char dev_id = ext_id & 0xf;
+
+        switch (dev_id) {
+            case GYRO_DEV_ID:
+                // char axis = ((ext_id & 0xf0) >> 4) - GYRO_PORT;
+
+                for(int axis = 1; axis < 4; axis++){
+                    if(DATA_TYPE_FLOAT == data_type[axis - 1]){
+                        if(GYRO_AXE_X == axis){
+                            data_gyro.x_ = *(float*)(makeblock_response_msg+4 + 5*(axis - 1));
+                        }
+                        if(GYRO_AXE_Y == axis){
+                            data_gyro.y_ = *(float*)(makeblock_response_msg+4 + 5*(axis - 1));
+                        }
+                        if(GYRO_AXE_Z == axis){
+                            data_gyro.z_ = *(float*)(makeblock_response_msg+4 + 5*(axis - 1));
+                        }
+                    }
+                }
+                ret = 0;
+
+            default:
+                break;
+        }
+    }
+
+    // for(int eraser = 0; eraser < MKBLK_MAX_MSG_SIZE; eraser++){
+    //     makeblock_response_msg[eraser] = 0x0;
+    // }
+    return ret;
+}
+
+float get_gyro_x(){
+    return data_gyro.x_;
+}
+
+float get_gyro_y(){
+    return data_gyro.y_;
+}
+
+float get_gyro_z(){
+    return data_gyro.z_;
+}
+
+float get_uss(int port){
+    return data_uss[port-1].distance_cm;
+}
+
+int request_data(const int fd, const char *s, const int buffer_size){
+    return write(fd, s, buffer_size);
+}
+
+int request_gyro_all_axes(const int fd){
+    return request_gyro(fd, 0);
+}
+
+int request_gyro(const int fd, char axis){
+    char ext_id_gyro = (((GYRO_PORT+axis)<<4)+GYRO_DEV_ID);
+    char gyro_msg[HEADER_MSG_SIZE + GYRO_MSG_SIZE] 
+    =   {0xff, 0x55, GYRO_MSG_SIZE, 
+        ext_id_gyro, ACTION_GET, GYRO_DEV_ID, GYRO_PORT, axis};
+
+    // fflush(stdout);
+    // ret = write (fd, gyro_msg, HEADER_MSG_SIZE + GYRO_MSG_SIZE);
+
+    // serialFlush(fd);
+    // delay(3);
+
+    // receive_msg(fd);
+
+    // char data[5];
+    // decode_data(data);
+    // if(DATA_TYPE_FLOAT == data[0]){
+    //     ret = *(float*)(data+1);
+    // } else {
+    //     ret = 0.0;
+    // }
+
+    return request_data(fd, gyro_msg, HEADER_MSG_SIZE + GYRO_MSG_SIZE);
+}
+
+int request_uss(const int fd, char port){
+
+    char ext_id_uss = ((port<<4)+USS_DEV_ID);
+    char uss_msg[HEADER_MSG_SIZE + USS_MSG_SIZE] 
+    =   {0xff, 0x55, USS_MSG_SIZE, 
+        ext_id_uss, ACTION_GET, USS_DEV_ID, port};
+
+    return request_data(fd, uss_msg, HEADER_MSG_SIZE + USS_MSG_SIZE);
+}
+
+void receive_msg(int fd){
+    int ii = 0; 
+
+    while (serialDataAvail (fd)){
+        makeblock_response_msg[ii] = serialGetchar (fd);
+        printf (" -> %3d", makeblock_response_msg[ii]) ;
+        fflush (stdout) ;
+
+        // In the case of USS sensor the header is send before the USS data is there
+        // therefore we need to wait for the data
+        if (2 == ii && (USS_DEV_ID == (makeblock_response_msg[ii]&0xf))){
+            delay(13);
+        }
+
+        ii++;
+    }
+
+    if(ii >= MAKEBLOCK_MSG_SIZE ){
+        decode_data();
+        printf ("\ndata_received of size: %d", ii) ;
+    }
+
+}
